354779151b693aa598fb5b6a4ad57c4e1e8d4b89
kyle-mcnaught <60240503+kyle-mcnaught@users.noreply.github.com>
2020-11-30 01:17:36 -0500

    Layout script added
    Added a script to create the layout for the tableau

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -11,10 +11,11 @@ public class Prospector : MonoBehaviour {
 
 	[Header("Set in Inspector")]
 	public TextAsset			deckXML;
-
+	public TextAsset              layoutXML;
 
 	[Header("Set Dynamically")]
 	public Deck					deck;
+	public Layout                 layout;
 
 	void Awake(){
 		S = this;
@@ -26,11 +27,12 @@ public class Prospector : MonoBehaviour {
 		
 		Deck.Shuffle(ref deck.cards);
  
-      	Card c;
-      	for (int cNum=0; cNum<deck.cards.Count; cNum++) {
-        	c = deck.cards[cNum];
-        	c.transform.localPosition = new Vector3( (cNum%13)*3, cNum/13*4, 0 );
+      	//Card c;
+      	//for (int cNum=0; cNum<deck.cards.Count; cNum++) {
+        	//c = deck.cards[cNum];
+        	//c.transform.localPosition = new Vector3( (cNum%13)*3, cNum/13*4, 0 );
+    	layout = GetComponent<Layout>();  // Get the Layout component
+        layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it
     }
 	}
 
-}
