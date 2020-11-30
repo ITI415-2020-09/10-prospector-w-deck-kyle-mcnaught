c9361ec5f7f7461dbdd3f3389fa53669d3b934b9
kyle-mcnaught <60240503+kyle-mcnaught@users.noreply.github.com>
2020-11-30 14:49:07 -0500

    CardProspector
    Created the CardProspector script and added it to PrefabCard

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -16,6 +16,7 @@ public class Prospector : MonoBehaviour {
 	[Header("Set Dynamically")]
 	public Deck					deck;
 	public Layout                 layout;
+	public List<CardProspector>    drawPile;
 
 	void Awake(){
 		S = this;
@@ -33,6 +34,17 @@ public class Prospector : MonoBehaviour {
         	//c.transform.localPosition = new Vector3( (cNum%13)*3, cNum/13*4, 0 );
     	layout = GetComponent<Layout>();  // Get the Layout component
         layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it
+        
+        drawPile = ConvertListCardsToListCardProspectors( deck.cards );
     }
-	}
+    List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD) {
+        List<CardProspector> lCP = new List<CardProspector>();
+        CardProspector tCP;
+        foreach( Card tCD in lCD ) {
+            tCP = tCD as CardProspector;                                     // a
+            lCP.Add( tCP );
+        }
+        return( lCP );
+    }
+   }
 
