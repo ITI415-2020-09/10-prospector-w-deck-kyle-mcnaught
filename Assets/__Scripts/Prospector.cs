2049302d80f744ba809f09f333668d02b93973fa
kyle-mcnaught <60240503+kyle-mcnaught@users.noreply.github.com>
2020-11-30 21:48:06 -0500

    Created a score
    The game now creates a score for the player

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -207,6 +207,7 @@ public class Prospector : MonoBehaviour {
             MoveToDiscard(target); // Moves the target to the discardPile
             MoveToTarget(Draw());  // Moves the next drawn card to the target
             UpdateDrawPile();     // Restacks the drawPile
+            ScoreManager.EVENT(eScoreEvent.draw);
             break;
 
         case eCardState.tableau:
@@ -226,6 +227,7 @@ public class Prospector : MonoBehaviour {
              tableau.Remove(cd); // Remove it from the tableau List
              MoveToTarget(cd);  // Make it the target card
              SetTableauFaces();
+             ScoreManager.EVENT(eScoreEvent.mine);
             break;
       }
       CheckForGameOver();
@@ -259,9 +261,11 @@ public class Prospector : MonoBehaviour {
     // Called when the game is over. Simple for now, but expandable
     void GameOver(bool won) {
         if (won) {
-            print ("Game Over. You won! :)");
+            //print ("Game Over. You won! :)");
+            ScoreManager.EVENT(eScoreEvent.gameWin);
         } else {
-            print ("Game Over. You Lost. :(");
+            //print ("Game Over. You Lost. :(");
+            ScoreManager.EVENT(eScoreEvent.gameLoss);
         }
         // Reload the scene, resetting the game
         SceneManager.LoadScene("__Prospector_Scene_0");
